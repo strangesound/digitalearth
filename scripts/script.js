c4b4486f1d719041100c810936f15c0e38a108a2
Arseny Petrovich <strangesound@gmail.com>
2020-11-14 01:29:51 +0300

    hideArrowsMediaQuery

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ var mySwiper = new Swiper('.swiper-container', {
     loop: true,
     // autoHeight: true,
     setWrapperSize: true,
-    
+
     // If we need pagination
     pagination: {
         el: '.swiper-pagination',
@@ -16,12 +16,46 @@ var mySwiper = new Swiper('.swiper-container', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
+    breakpoints: {
+        1200: {
+            
+        },
+        1024: {
+           
+        },
+        768: {
+            
+        },
+        675: {
+            
+        }
     },
-})
+    on: {
+        init: function () {
+            checkArrow();
+        },
+        resize: function () {
+            checkArrow();
+        }
+    }
+});
+
+
+
+
+function checkArrow() {
+    var swiperPrev = document.querySelector('.swiper-button-prev');
+    var swiperNext = document.querySelector('.swiper-button-next');
+    if (window.innerWidth < 1024) {
+        console.log('Success', window.innerWidth);
+        swiperPrev.style.display = 'block';
+        swiperNext.style.display = 'block';
+    } else {
+        swiperPrev.style.display = 'none';
+        swiperNext.style.display = 'none';
+    }
+}
+
 
 
 // Safari scroll behavior
@@ -36,7 +70,7 @@ function SmoothVerticalScrolling(e, time, where) {
 }
 
 function SVS_B(eAmt, where) {
-    if(where == "center" || where == "")
+    if (where == "center" || where == "")
         window.scrollBy(0, eAmt / 2);
     if (where == "top")
         window.scrollBy(0, eAmt);
